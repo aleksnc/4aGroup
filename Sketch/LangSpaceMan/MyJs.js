f2575383b8980e816d41b0f4b31a00e0c51f6f75
alexandr_abramov <aleksnc@mail.ru>
2017-05-26 12:25:33 +0300

    Anim lang
    flying astronaut

--- Sketch/LangSpaceMan/MyJs.js
+++ Sketch/LangSpaceMan/MyJs.js
@@ -1,36 +1,88 @@
+arr = [];
+var ltr = true;
+var sin = true;
+var Point = $('.langSwitch');
+var NewY;
+var NewX;
+var PoinPosX = Point.position().left;
+var PoinPosY = Point.position().top;
+var i = 0;
+var vectorY = 1;
+var rotate =0;
 
-$(document).ready(function(){
+arr = [PoinPosX, PoinPosY, 0.7];
+var timerId = setInterval(function () {
+    (ltr == true) ? i += 0.1 : i -= 0.1;
+    vectorY = (sin == true) ? 1 : -1;
 
-        var x = -(e.pageX + this.offsetLeft) / 40;
-        var y = -(e.pageY + this.offsetTop) / 40;
-        var xr = -(e.pageX + this.offsetLeft) / 20;
-        var yr = -(e.pageY + this.offsetTop) / 20;
+    NewX = arr[0] + i * 3;
 
-        var xc = -(e.pageX + this.offsetLeft) / 30;
-        var yc = -(e.pageY + this.offsetTop) / 30;
+    NewY = (arr[1] - Math.abs(NewX - arr[0]) * Math.sin(arr[2] * vectorY));
 
-        $('.planet-earth').css({
-            'top': y+70 + 'px ' ,
-            'left': x+70 + 'px'});
-
-        $('.rocketq').css({
-            'top': yr+150 + 'px ' ,
-            'left': xr+150 + 'px'});
-
-        $('.cloud1').css({
-            'top': yc+150 + 'px ' ,
-            'left': xc+10 + 'px'});
-
-        $('.cloud2').css({
-            'top': yc/0.4+60 + 'px ' ,
-            'left': xc/0.4+250 + 'px'});
-
-        $('.cloud3').css({
-            'top': yc/1+200 + 'px ' ,
-            'left': xc/1+200 + 'px'});
-
-        $('.cloud4').css({
-            'top': yc/1.5+170 + 'px ' ,
-            'left': xc/1.5+370 + 'px'});
+    Point.css({
+        'left': NewX,
+        'top': NewY
     });
+
+    rotate+=i/180;
+    $('.spaceman').css({
+        'transform': 'rotate(0'+rotate+'deg)'
+    });
+    Collision();
+}, 1000 / 120);
+
+function Collision() {
+    var a = Point;
+    var b = $('.langSwitch-wrapper');
+    //
+    //console.log(a.position().top + 72);
+    //var bpos = b.position().top + b.height();
+    //console.log(bpos);
+    //console.log(ltr);
+
+    if (a.position().left + 90 >= b.position().left + b.width()) {
+        arr[0] = NewX;
+        arr[1] = NewY;
+        i = 0;
+        arr[2] += 0.1;
+        ltr = false;
+
+    }
+    if (a.position().left <= b.position().left) {
+        ltr = true;
+        i = 0;
+        i = 0;
+        arr[0] = NewX;
+        arr[1] = NewY;
+        arr[2] += 0.1;
+    }
+
+    if (a.position().top <= b.position().top) {
+        sin = false;
+        i = 0;
+        arr[0] = NewX;
+        arr[1] = NewY;
+        arr[2] -= 0.1;
+    }
+
+    if (a.position().top + 72 >= b.position().top + b.height()) {
+        sin = true;
+        i = 0;
+        arr[0] = NewX;
+        arr[1] = NewY;
+        arr[2] -= 0.1;
+    }
+}
+
+
+$('.langSwitch').click(function(){
+    $(this).find('.spaceman').toggleClass('spaceman--en');
+    $('.text').empty();
+    if($('.spaceman').hasClass('spaceman--en')){
+        $('.text').html('click on the astronaut');
+    } else{
+        $('.text').html('нажми на космонавта');
+    }
 });
+
+
